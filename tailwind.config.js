5c8843d337dc94796079e4b210c413573671c68e
RohanScripts <shelarrohan1810@gmail.com>
2024-11-24 15:59:42 +0530

    Add new dependencies and implement AOS and Slick Carousel in Hero component

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        themeyellow: "f5e60d",
+        themepurple: "502ec3",
+      },
+    },
   },
   plugins: [],
 };
